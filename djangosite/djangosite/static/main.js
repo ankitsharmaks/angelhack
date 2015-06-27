0327e2630aa22ca9911a1588d850d63e5317c355
Jeremy Cleveland <jdc@jdccreations.com>
2015-06-27 15:53:35 -0700

    Basic Respoke integration working.

--- djangosite/djangosite/static/main.js
+++ djangosite/djangosite/static/main.js
@@ -1,3 +1,52 @@
 
 $(document).ready(function() {
+    var appid = "cd39e41d-8f45-4406-8218-e7f74ffae2f9";
+    var endpointId = user;
+    var group;
+
+    var client = respoke.createClient({
+        appId: appid,
+        developmentMode: true
+    });
+
+    console.log("Connecting...");
+    client.connect({
+        endpointId: endpointId // your username is the endpoint
+    });
+
+    // "connect" event fired after successful connection to Respoke
+    client.listen('connect', function() {
+        console.log("Connected to Respoke as \"" + endpointId + "\"");
+        // Update group status message
+        console.log('Joining group ' + group_id);
+        // Automatically join the group once connected
+        client.join({
+            id: group_id,
+            onSuccess: function(grp) {
+                console.log('Joined group ' + grp.id);
+                group = grp;
+            },
+            onMessage: function(evt) {
+                $("#messages").append(
+                    ""+evt.message.endpointId+": " + evt.message.message + "<br>"
+                );
+            }
+        });
+    });
+    // Send message
+
+    $("#chat-form").submit(function(event) {
+        event.preventDefault();
+        $chat_form_input = $("#chat-form input");
+        var groupMsg = $chat_form_input.val();
+        console.log(groupMsg);
+        $chat_form_input.val('');
+        group.sendMessage({
+            message: groupMsg
+        });
+        $("#messages").append(
+            ""+ endpointId+": "+ groupMsg + "<br>"
+        );
+        return false;
+    });
 });
--- djangosite/djangosite/views.py
+++ djangosite/djangosite/views.py
@@ -11,7 +11,8 @@ def index(request):
 
 def chat(request):
     user_param = request.GET['user']
-    if request.method == 'GET' and user_param:
+    group_param = request.GET['group']
+    if request.method == 'GET' and user_param and group_param:
         request.session['user'] = user_param
-        return render(request, 'chat.html', {'user' : user_param})
+        return render(request, 'chat.html', {'user' : user_param, 'group' : group_param})
     return HttpResponseNotFound('<h1>Page not found</h1>')
