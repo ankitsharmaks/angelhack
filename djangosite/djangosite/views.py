e03739ec717653294ddcbe33fe27e36fb7e01c5c
Jeremy Cleveland <jdc@jdccreations.com>
2015-06-27 13:14:05 -0700

    Add basic template to system.

--- djangosite/djangosite/views.py
+++ djangosite/djangosite/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, render
 import datetime
 
 def index(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    #now = datetime.datetime.now()
+    return render(request, 'index.html', {})
+    #html = "<html><body>It is now %s.</body></html>" % now
+    #return HttpResponse(html)
